524c0c534e2a59bfbac798d4215865a2f684aa30
Kirill Averkiev <akirill1942@me.com>
2023-05-04 14:33:31 +0300

    Fixed path and query items test

--- Tests/EnkaNetworkKitTests/EnkaNetworkEndpointTests.swift
+++ Tests/EnkaNetworkKitTests/EnkaNetworkEndpointTests.swift
@@ -6,7 +6,9 @@ final class EnkaNetworkEndpointTests: XCTestCase {
     let url: URL = URL(string: "https://example.com")!
     
     func testPaths() throws {
-        XCTAssertEqual(url.appending(paths: EnkaNetworkEndpoint.uid(uid: "123", onlyInfo: true).endpointPathComponents).absoluteString.removingPercentEncoding,
-                       "https://example.com/uid/123/?info")
+        
+        let endpoint = EnkaNetworkEndpoint.uid(uid: "123", onlyInfo: true)
+        
+        XCTAssertEqual(url.appending(paths: endpoint.endpointPathComponents).appending(queryItems: endpoint.enpointQuery).absoluteString, "https://example.com/uid/123?info")
     }
 }
