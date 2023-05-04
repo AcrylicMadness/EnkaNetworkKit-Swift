3be5ae8a6e77c90f5ae317612097e831c8e1eec7
AcrylicMadness <akirill1942@me.com>
2023-05-05 00:52:33 +0300

    Fix data(from: )

--- Sources/EnkaNetworkKit/Utils/URLSession+Extensions.swift
+++ Sources/EnkaNetworkKit/Utils/URLSession+Extensions.swift
@@ -5,13 +5,13 @@ import FoundationNetworking
 
 extension URLSession {
     func object<T: Codable>(from url: URL, withType type: T.Type) async throws -> T {
-        let (responseData, _) = try await self.data(from: url)
+        let (responseData, _) = try await self.data(from: url, delegate: nil)
         let decoder = JSONDecoder() 
         return try decoder.decode(T.self, from: responseData)
     }
     
     func object<T: Codable>(for request: URLRequest, withType type: T.Type) async throws -> T {
-        let (responseData, _) = try await self.data(for: request)
+        let (responseData, _) = try await self.data(for: request, delegate: nil)
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: responseData)
     }
